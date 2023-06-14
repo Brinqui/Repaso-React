b5ec2a5c91975306ac9327f3a12ea586b49ca570
Alvaro <arincon2709@gmail.com>
2023-06-14 23:49:30 +0200

    Arreglo

--- Card-Dualiza/src/components/Card/Card.jsx
+++ Card-Dualiza/src/components/Card/Card.jsx
@@ -1,94 +1,93 @@
-import React from "react";
-import script from "./script";
+import React, { useEffect } from "react";
 const Card = () => {
-  
+  useEffect(() => {
+    document.getElementById("cards").onmousemove = (e) => {
+      for (const card of document.getElementsByClassName("card")) {
+        const rect = card.getBoundingClientRect(),
+          x = e.clientX - rect.left,
+          y = e.clientY - rect.top;
+
+        card.style.setProperty("--mouse-x", `${x}px`);
+        card.style.setProperty("--mouse-y", `${y}px`);
+      }
+    };
+  }, []);
+
   return (
     <>
       <div id="cards">
-        <div class="card">
-          <div class="card-content">
-            <div class="card-image">
-              <i class="fa-duotone fa-apartment"></i>
+        <div className="card">
+          <div className="card-content">
+            <div className="card-image">
             </div>
-            <div class="card-info-wrapper">
-              <div class="card-info">
-                <i class="fa-duotone fa-apartment"></i>
-                <div class="card-info-title">
+            <div className="card-info-wrapper">
+              <div className="card-info">
+                <div className="card-info-title">
                   <h3>Card 1</h3>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <div class="card">
-          <div class="card-content">
-            <div class="card-image">
-              <i class="fa-duotone fa-unicorn"></i>
+        <div className="card">
+          <div className="card-content">
+            <div className="card-image">
             </div>
-            <div class="card-info-wrapper">
-              <div class="card-info">
-                <i class="fa-duotone fa-unicorn"></i>
-                <div class="card-info-title">
+            <div className="card-info-wrapper">
+              <div className="card-info">
+                <div className="card-info-title">
                   <h3>Card 2</h3>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <div class="card">
-          <div class="card-content">
-            <div class="card-image">
-              <i class="fa-duotone fa-blender-phone"></i>
+        <div className="card">
+          <div className="card-content">
+            <div className="card-image">
             </div>
-            <div class="card-info-wrapper">
-              <div class="card-info">
-                <i class="fa-duotone fa-blender-phone"></i>
-                <div class="card-info-title">
+            <div className="card-info-wrapper">
+              <div className="card-info">
+                <div className="card-info-title">
                   <h3>Card 3</h3>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <div class="card">
-          <div class="card-content">
-            <div class="card-image">
-              <i class="fa-duotone fa-person-to-portal"></i>
+        <div className="card">
+          <div className="card-content">
+            <div className="card-image">
             </div>
-            <div class="card-info-wrapper">
-              <div class="card-info">
-                <i class="fa-duotone fa-person-to-portal"></i>
-                <div class="card-info-title">
+            <div className="card-info-wrapper">
+              <div className="card-info">
+                <div className="card-info-title">
                   <h3>Card 4</h3>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <div class="card">
-          <div class="card-content">
-            <div class="card-image">
-              <i class="fa-duotone fa-person-from-portal"></i>
+        <div className="card">
+          <div className="card-content">
+            <div className="card-image">
             </div>
-            <div class="card-info-wrapper">
-              <div class="card-info">
-                <i class="fa-duotone fa-person-from-portal"></i>
-                <div class="card-info-title">
+            <div className="card-info-wrapper">
+              <div className="card-info">
+                <div className="card-info-title">
                   <h3>Card 5</h3>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <div class="card">
-          <div class="card-content">
-            <div class="card-image">
-              <i class="fa-duotone fa-otter"></i>
+        <div className="card">
+          <div className="card-content">
+            <div className="card-image">
             </div>
-            <div class="card-info-wrapper">
-              <div class="card-info">
-                <i class="fa-duotone fa-otter"></i>
-                <div class="card-info-title">
+            <div className="card-info-wrapper">
+              <div className="card-info">
+                <div className="card-info-title">
                   <h3>Card 6</h3>
                 </div>
               </div>
